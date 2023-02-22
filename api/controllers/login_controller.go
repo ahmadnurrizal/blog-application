6b741a0d3569d72423e066a409d537efbea1241b
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2023-02-23 06:23:50 +0700

    add comment login controller

--- api/controllers/login_controller.go
+++ api/controllers/login_controller.go
@@ -29,12 +29,17 @@ func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// trim any whitespace and convert the email address to lowercase
 	user.Prepare()
+
+	// check that the email address and password meet the required validation criteria.
 	err = user.Validate("login")
 	if err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
+	// generate token
 	token, err := server.SignIn(user.Email, user.Password)
 	if err != nil {
 		formattedError := formaterror.FormatError(err.Error())
@@ -50,10 +55,13 @@ func (server *Server) SignIn(email, password string) (string, error) {
 
 	user := models.User{}
 
+	// query the database for a User record that matches the given email parameter. Take method to retrieve a single record that matches the query and assign to user variable
 	err = server.DB.Debug().Model(models.User{}).Where("email = ?", email).Take(&user).Error
 	if err != nil {
 		return "", err
 	}
+
+	// check if the password provided matches the password stored in the user.Password field
 	err = models.VerifyPassword(user.Password, password)
 	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
 		return "", err
